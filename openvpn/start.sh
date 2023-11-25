c4c6ef996b9d09941b1e0776b5e8803855f53c12
NoX1De <dixon0dw@gmail.com>
2023-11-24 22:21:30 -0500

    Move tun0 check and create to start.sh for cleaner approach.

--- openvpn/start.sh
+++ openvpn/start.sh
@@ -2,6 +2,16 @@
 # Forked from binhex's OpenVPN dockers
 set -e
 
+# check for presence of tun0 interface
+check_network=$(ifconfig | grep tun0 || true)
+if [[ ! -z "${check_network}" ]]; then
+        echo "Creating tun0 interface so openvpn can properly establish a vpn tunnel connection." | ts '%Y-%m-%d %H:%M:%.S'
+	mkdir /dev/net && mknod /dev/net/tun c 10 200 && chmod 666 /dev/net/tun
+        # Sleep so it wont 'spam restart'
+        sleep 10
+        exit 1
+fi
+
 # check for presence of network interface docker0
 check_network=$(ifconfig | grep docker0 || true)
 
